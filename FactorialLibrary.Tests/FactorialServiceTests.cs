058548cdb417e31c6c229b1a1e26f8ef9e5b572f
Eric <163595322+erobinson-mlis@users.noreply.github.com>
2026-01-22 22:04:04 +0000

    initial factorial project files

---
+++ FactorialLibrary.Tests/FactorialServiceTests.cs
@@ -0,0 +1,16 @@
+﻿namespace FactorialLibrary.Tests;
+
+using FactorialLibrary;
+
+public class FactorialServiceTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var result1 = FactorialService.Factorial(0);
+        Assert.Equal(1, result1);
+
+        var result2 = FactorialService.Factorial(1);
+        Assert.Equal(1, result2);
+    }
+}
---
+++ FactorialLibrary/FactorialService.cs
@@ -0,0 +1,12 @@
+﻿namespace FactorialLibrary;
+
+public class Class1
+{
+    public static int Factorial(int n) {
+        if(n==0 || n==1) { //   0! = 1,  1! = 1
+            return 1;
+        };
+        return n * Factorial(n-1); // e.g., 5! = 5 * 4!
+    }
+
+}
