317f67cb074de7793a106b5f67fe884734faea85
Eric <163595322+erobinson-mlis@users.noreply.github.com>
2026-01-22 22:54:19 +0000

    Changes to be committed: 	modified:   FactorialLibrary.Tests/FactorialServiceTests.cs 	modified:   FactorialLibrary/FactorialService.cs

--- FactorialLibrary.Tests/FactorialServiceTests.cs
+++ FactorialLibrary.Tests/FactorialServiceTests.cs
@@ -13,10 +13,14 @@ public class FactorialServiceTests
         var result2 = FactorialService.Factorial(1);
         Assert.Equal(1, result2);
 
-        var result4 = FactorialService.Factorial(7);
-        Assert.Equal(5040, result4);
-
         var result3 = FactorialService.Factorial(10);
         Assert.Equal(3628800, result3);
+
+        var result4 = FactorialService.Factorial(7);
+        Assert.Equal(5040, result4);
+        
+        // Assert.Throws<InvalidOperationException>(() => (
+        //     var result5 = FactorialService.Factorial(-1);
+        // ));
     }
 }
--- FactorialLibrary/FactorialService.cs
+++ FactorialLibrary/FactorialService.cs
@@ -3,9 +3,15 @@
 public class FactorialService
 {
     public static int Factorial(int n) {
+
+        if(n<0) {
+            throw new InvalidOperationException("Negative numbers do not have a factorial.");
+        }
+
         if(n==0 || n==1) { //   0! = 1,  1! = 1
             return 1;
-        };
+        }
+        
         return n * Factorial(n-1); // e.g., 5! = 5 * 4!
     }
 
